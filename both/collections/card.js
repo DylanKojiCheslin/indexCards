8f1616284b58148852c1ca0a70d6b40edacb734f
dylan <dylan.kojicheslin@gmail.com>
2016-01-20 02:39:40 -0700

    fixed 2

---
+++ both/collections/card.js
@@ -0,0 +1,34 @@
+Card = ManyModel.extendAndSetupCollection("card");
+
+Card.collection.allow({
+  insert: function(){
+    return false;
+  },
+  update: function(){
+    return false;
+  },
+  remove: function(){
+    return false;
+  }
+});
+
+Card.appendSchema({
+  "questions":{
+    type: String,
+  },
+  "answer":{
+    type: String,
+  },
+  "difficulty":{
+    type: Number,
+  },
+  //tags to be refrenced by ManyModel
+  // "tags":{
+  //   type:
+  // },
+  "createdBy":{
+    type: SimpleSchema.RegEx.Id,
+  },
+});
+
+//Card.methods({});
