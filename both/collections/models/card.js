1c0de2f2f3552f1a3d8e82f19c770b869546a3a3
dylan <dylan.kojicheslin@gmail.com>
2016-02-12 22:56:46 -0700

    fixed #12

--- both/collections/models/card.js
+++ both/collections/models/card.js
@@ -34,25 +34,44 @@ Card.meteorMethods.insertCard = new ValidatedMethod ({
   validate: function(){
       return Card.schema.validator();
     },
-  run: function( stuff ) {
-      Card.collection.insert(stuff);
+  run: function( doc ) {
+      doc.createdBy = Meteor.userId()
+      Card.collection.insert(doc);
     },
 });
 
 Card.meteorMethods.updateCard = new ValidatedMethod ({
   name: "Card.meteorMethods.updateCard",
+  mixins : [LoggedInMixin],
+  checkLoggedInError: {
+    error: 'notLogged',
+    message: 'You need to be logged in to call this method',
+    reason: 'You need to login'
+  },
   validate: function(){
     return Card.schema.validator();
   },
-  run: function( stuff ){
-    var documentId = stuff._id;
-    var modifier = stuff.modifier;
+  run: function( doc ){
+    var documentId = doc._id;
+    var modifier = doc.modifier;
     var callbackResponse = {
       toastrTitle:"success",toastrMessage:"Card Updated"
     };
+    if ( ! this.isSimulation) {
+      var realThing = Card.collection.findOne(
+        documentId,
+        { fields: { "createdBy": 1, _id: 1 }}
+      );
+      if (realThing.createdBy !== Meteor.userId()) {
+         throw new Meteor.Error("access denied", "can only edit if you made it");
+      }
     Card.collection.update(documentId, modifier);
         return callbackResponse;
+    }
   },
+});
+
+Card.methods({
 });
 
 Card.collection.allow({
