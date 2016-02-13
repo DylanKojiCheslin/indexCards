1dc8ce3584e38d105a16b9a47d2827ed20290b11
dylan <dylan.kojicheslin@gmail.com>
2016-02-12 18:48:19 -0700

    fixed #13

--- server/insertCard.js
+++ server/insertCard.js
@@ -1,9 +1,16 @@
-Meteor.methods({
-  insertCard:function(doc){
-     check(doc, Card.collection.simpleSchema());
-     if (!this.userId) {
-        throw new Meteor.Error("access denied", "please log in")
-     }
-     Card.collection.insert(doc);
-  }
+const method = new ValidatedMethod({
+  name: 'insertCard',
+  mixins : [LoggedInMixin],
+  checkLoggedInError: {
+    error: 'test',
+    code: 'notLogged',
+    message: 'You need to be logged in to call this method',
+    reason: 'You need to login'
+  },
+  validate(doc) {
+    check(doc, Card.collection.simpleSchema());
+  },
+  run(doc) {
+    Card.collection.insert(doc);
+  },
 });
